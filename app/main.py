b5c8f08582cacf8565e563e00560f56fcdf4b778
Farshad <farshadsafarkhanloo@gmail.com>
2026-09-19 15:25:34 -0400

    feat: add database and user model

--- app/main.py
+++ app/main.py
@@ -1,5 +1,10 @@
 from fastapi import FastAPI
 
+from app.database.database import Base, engine
+from app.models.user import User
+
+Base.metadata.create_all(bind=engine)
+
 app = FastAPI(
     title="LocalDrop",
     description="Local network file sharing made simple.",
